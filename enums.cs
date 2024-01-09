1ce26b7e682062dea2c031645b65f325d4b1a815
Zülal <96519985+ZulalMeydan35@users.noreply.github.com>
2024-01-09 15:51:00 +0300

    enums.cs

---
+++ enums.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yilanoyunu
+{
+    public enum Direction
+    {
+        Right,
+        Left,
+        Up,
+        Down,
+    }
+}
